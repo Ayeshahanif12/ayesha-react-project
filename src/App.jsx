08f11d0bcecba6c3f290cd9eb4a6fad5a29fda0f
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2024-10-21 19:59:51 +0500

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  {useState,useEffect}  from 'react'
+import { ChakraProvider } from '@chakra-ui/react';
+import { Box,Button,Text,grid,Flex } from '@chakra-ui/react';
+import SpecialFood from './components/SpecialFood';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const gridSize = 20 
+const SnakeGame = ()=>{
+ const [snake,setSnake] = useState([{ x: 10, y: 10 }]);
+const[direction,setDirection] = useState('right');
+const [score,setScore] = useState(0);
+const [gameInterval,setGameInterval]=useState;
+const [gameSpeed,setGameSpeed] = useState(10);
+const [gameStarted,setGameStarted] = useState(false);
+const [highScore,setHighScore] = useState(0);
 }
+
 
 export default App
---
+++ src/components/SpecialFood.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const SpecialFood = ({ specialFood }) => {
+  return (
+    <Box
+      bg="gold"
+      border="2px solid red"
+      gridColumn={specialFood.x}
+      gridRow={specialFood.y}
+      w="20px"
+      h="20px"
+    />
+  );
+};
+
+export default SpecialFood;
