1bcb5a7ec3b945394141331c6683ff9e54109360
Ayeshhanif12 <ayeshahanif.0317@gmail.com>
2024-10-22 18:39:13 +0500

    work

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import  {useState,useEffect}  from 'react'
 import { ChakraProvider } from '@chakra-ui/react';
 import { Box,Button,Text,grid,Flex } from '@chakra-ui/react';
 import SpecialFood from './components/SpecialFood';
+import GameOver from './components/GameOver';
 
 const gridSize = 20 
 const SnakeGame = ()=>{
@@ -11,8 +12,6 @@ const [score,setScore] = useState(0);
 const [gameInterval,setGameInterval]=useState;
 const [gameSpeed,setGameSpeed] = useState(10);
 const [gameStarted,setGameStarted] = useState(false);
-const [highScore,setHighScore] = useState(0);
 }
+ 
 
-
-export default App
