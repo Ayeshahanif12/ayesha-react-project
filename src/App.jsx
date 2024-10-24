acb77336046a5712bbffa50368ccafcd19e948a7
Ayeshhanif12 <ayeshahanif.0317@gmail.com>
2024-10-24 18:56:46 +0500

    snake

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,38 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { Box,Button,Text,grid,Flex } from '@chakra-ui/react';
 import SpecialFood from './components/SpecialFood';
 import GameOver from './components/GameOver';
-import App from './main';
+
 
 const gridSize = 20 
-const SnakeGame = ()=>{
+const App = ()=>{
  const [snake,setSnake] = useState([{ x: 10, y: 10 }]);
 const[direction,setDirection] = useState('right');
 const [score,setScore] = useState(0);
-const [gameInterval,setGameInterval]=useState;
+const [gameInterval,setGameInterval]=useState(null);
 const [gameSpeed,setGameSpeed] = useState(10);
 const [gameStarted,setGameStarted] = useState(false);
-}
+ 
+useEffect (()=>{
+generatefood(); 
+}, []);
+}   
+ useEffect(()=>{
+    if (gameStarted) {
+        const interval = setInterval(() => {
+            move();
+        }, gameSpeed);
+        return () => clearInterval(interval);
+    }
+}, [gameStarted, snake, direction, gameSpeed]);
+
+
+    useEffect(()=>{
+        if(gameStarted){
+            const interval = setInterval(()=>{
+                move();
+            })
+            return()=> clearInterval(interval)
+        }
+    },[gameStarted,direction,snake,speed])
  export default App
 
