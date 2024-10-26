fd683cffb0f768f9d23c8597c94c5b1482450543
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2024-10-26 15:15:54 +0500

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Box, Button, Text, grid, Flex } from '@chakra-ui/react';
 import SpecialFood from './components/SpecialFood';
 
 const gridSize = 20
-const SnakeGame = () => {
+const App = () => {
   const [snake, setSnake] = useState([{ x: 10, y: 10 }]);
   const [direction, setDirection] = useState('right');
   const [score, setScore] = useState(0);
@@ -12,7 +12,10 @@ const SnakeGame = () => {
   const [gameSpeed, setGameSpeed] = useState(10);
   const [gameStarted, setGameStarted] = useState(false);
   const [highScore, setHighScore] = useState(0);
-}
+}   
+useEffect(()=>{
+  
+})
 
 
 export default App 
