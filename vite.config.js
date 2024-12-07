f037eb63f3edcb10355a9c048b6b84be5c5a0edf
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2024-12-07 20:06:41 +0500

    ff

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<ayesha-react-project>/',
+  base: '/ayesha-react-project/',
+
 })
