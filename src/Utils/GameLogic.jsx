3100c7c25afd469958684ee9f74dad730aec6b89
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2024-12-07 18:49:09 +0500

    ff

--- src/Utils/GameLogic.jsx
+++ src/Utils/GameLogic.jsx
@@ -1,26 +1,25 @@
 export const generateFood = (gridSize, snake) => {
-    debugger;
-    let newFood;
-    do {
-      newFood = {
-        x: Math.floor(Math.random() * gridSize) + 1,
-        y: Math.floor(Math.random() * gridSize) + 1,
-      };
-    } while (snake.some(segment => segment.x === newFood.x && segment.y === newFood.y));
-  
-    return newFood;
-  };
-  
-  export const checkCollisions = (head, gridSize, snake) => {
-    return (
-      head.x < 1 || head.x > gridSize || 
-      head.y < 1 || head.y > gridSize ||
-      snake.slice(1).some(segment => segment.x === head.x && segment.y === head.y) // Check against all segments except the head
-    );
-  };
-  
-  export const increaseSpeed = (currentSpeed) => {
-    const minimumSpeed = 50;
-    return currentSpeed > minimumSpeed ? currentSpeed - 5 : minimumSpeed;
-  };
-  
+ 
+  let newFood;
+  do {
+    newFood = {
+      x: Math.floor(Math.random() * gridSize) + 1,
+      y: Math.floor(Math.random() * gridSize) + 1,
+    };
+  } while (snake.some(segment => segment.x === newFood.x && segment.y === newFood.y));
+
+  return newFood;
+};
+
+export const checkCollisions = (head, gridSize, snake) => {
+  return (
+    head.x < 1 || head.x > gridSize || 
+    head.y < 1 || head.y > gridSize ||
+    snake.slice(1).some(segment => segment.x === head.x && segment.y === head.y) // Check against all segments except the head
+  );
+};
+
+export const increaseSpeed = (currentSpeed) => {
+  const minimumSpeed = 50;
+  return currentSpeed > minimumSpeed ? currentSpeed - 5 : minimumSpeed;
+};
